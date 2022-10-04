f0bdfc98cd022f039c78a2af085689cba616998c
陳柏衡 <b07902114@ntu.edu.tw>
2022-10-05 01:49:27 +0800

    Parse for specific file

--- README.md
+++ README.md
@@ -5,16 +5,25 @@ A fully working sample Maven project that parses and generates code with [JavaPa
 
 This is targeted at people without [Maven](https://maven.apache.org/) experience.
 
+## Build
+
 To build it, you will need to download and unpack the latest (or recent) version of Maven (https://maven.apache.org/download.cgi)
 and put the `mvn` command on your path.
 Then, you will need to install a Java 1.8 (or higher) JDK (not JRE!), and make sure you can run `java` from the command line.
-Now you can run `mvn clean install` and Maven will compile your project, 
-an put the results it in two jar files in the `target` directory.
+
+Now you can run 
+
+```bash
+$ mvn clean install
+```
+
+Maven will compile your project,  an put the results it in two jar files in the `target` directory.
+
+## Run
+
 If you like to run from the command line,
-execute `java -jar target/javaparser-maven-sample-1.0-SNAPSHOT-shaded.jar`.
+execute
 
-How you run this code is up to you, but usually you would start by using an IDE like [NetBeans](https://netbeans.org/), [Intellij IDEA](https://www.jetbrains.com/idea/), or [Eclipse](https://eclipse.org/ide/).
-
-The Maven dependencies may lag behind the official releases a bit.
-
-If you notice some problems with this setup, please open an issue.
+```bash
+java -jar target/javaparser-maven-1.0-SNAPSHOT-shaded.jar /Absolute/Path/To/The/Java/File/You/Want/To/Parse
+```
---
+++ src/main/java/com/yourorganization/maven_sample/ClassParser.java
@@ -0,0 +1,90 @@
+package com.yourorganization.maven_sample;
+
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.utils.CodeGenerationUtils;
+import com.github.javaparser.utils.SourceRoot;
+import com.github.javaparser.ast.body.*;
+import java.util.ArrayList;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Some code that uses JavaParser.
+ */
+public class ClassParser {
+    public static void main(String[] args) {
+        Path path = Paths.get(args[0]);
+        String class_path = path.getFileName().toString();
+        String package_path = path.getParent().toString();
+        
+        SourceRoot sourceRoot = new SourceRoot(CodeGenerationUtils.mavenModuleRoot(ClassParser.class).resolve(package_path));
+        
+        System.out.println("Parsing " + args[0]);
+        CompilationUnit cu = sourceRoot.parse("", class_path);
+        
+        System.out.println("\n--- Attributes ---");
+        cu.findAll(FieldDeclaration.class).forEach(
+            attribute -> {
+                attribute.findAll(VariableDeclarator.class).forEach(
+                    variable -> {
+                        StringBuilder sb = new StringBuilder();
+                        attribute.getModifiers().forEach(modifier -> {
+                            switch (modifier.getKeyword().asString()) {
+                                case "public":
+                                    sb.append("+ ");
+                                    break;
+                                case "private":
+                                    sb.append("+ ");
+                                    break;
+                                case "protected":
+                                    sb.append("# ");
+                                    break;
+                                default:
+                                    break;
+                            }
+                        });
+                        sb.append(variable.getNameAsString() + " : " + variable.getTypeAsString());
+                        String res = sb.toString();
+                        System.out.println(res);
+                    }
+                ); 
+            }
+        );
+
+        System.out.println("\n--- Methods ---");
+
+        cu.findAll(MethodDeclaration.class).forEach(
+            method -> {
+                StringBuilder sb = new StringBuilder();
+                method.getModifiers().forEach(modifier -> {
+                    switch (modifier.getKeyword().asString()) {
+                        case "public":
+                            sb.append("+ ");
+                            break;
+                        case "private":
+                            sb.append("+ ");
+                            break;
+                        case "protected":
+                            sb.append("# ");
+                            break;
+                        default:
+                            break;
+                    }
+                });
+                sb.append(method.getNameAsString());
+                sb.append("(");
+                // String prefix = "";
+                ArrayList<String> parameters = new ArrayList<>();
+                method.getParameters().forEach(parameter -> {
+                    parameters.add(parameter.getNameAsString() + " : " + parameter.getTypeAsString());
+                });
+                sb.append(String.join(", ", parameters));
+                sb.append(") : ");
+                sb.append(method.getType().asString());
+                String res = sb.toString();
+                System.out.println(res);
+            }
+        );
+    }
+}
